84a2eea35421f3225b1cdb9710bf69ab4b2d55fb
Ahmad Tash Douqa <100798368+shamstash@users.noreply.github.com>
2023-08-25 02:26:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ let ispOut = document.querySelector("#result4 h2")
 
 
 const getLocation = async(ip) => {
-    const res = await axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_BH68ACXzteSjksNqGt0OsdVUNtSx1&ipAddress=${ip}`)
+    const res = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_BH68ACXzteSjksNqGt0OsdVUNtSx1&ipAddress=${ip}`)
     console.log(res.data)
     ipAddressOut.innerText = ip
     locationOut.innerText = res.data.location.region
     timezoneOut.innerText = res.data.location.timezone
     ispOut.innerText = res.data.isp
+    mapfunc(res.data.location.lng,res.data.location.lat)
+    console.log(res.data.location.lng,res.data.location.lat)
 }
 
     
@@ -22,12 +24,22 @@ subButton.addEventListener("click", function(){
 })
 
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map;
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+function mapfunc(long, lat) {
+    
+    if (map) {
+        map.off(); 
+        map.remove(); 
+    }
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+   
+    map = L.map('map', {zoomControl:false}).setView([lat, long], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([lat, long]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();}
